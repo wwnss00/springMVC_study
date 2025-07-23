53595d36a085d18e5b6ff980a615df3f16df887e
wwnss00 <wns208500@gmail.com>
2025-07-23 18:30:34 +0900

    프론트 컨트롤러 도입-v1

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -1,21 +1,20 @@
-package hello.servlet.web.servletMVC;
+package hello.servlet.web.frontcontroller.v1.controller;
 
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "mvcMemberServlet", urlPatterns = "/servlet-mvc/members/new-form")
-public class MvcMemberServlet extends HttpServlet {
+public class MemberFormControllerV1 implements ControllerV1 {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String viewPath = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+
     }
 }
